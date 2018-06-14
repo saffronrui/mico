252a0f797b1e16a966a0bbc362e35b2b40bddc93
Administrator <Administrator@USER-20180118VO>
2018-06-14 20:31:01 +0800

    new project structure
    1.new project structure
    
    Change-Id: I452d9575dc51b811a59107c662842560320c9fbd

---
+++ MiCO_A_v3.2.0/demos/helloworld/sem_os_APP.h
@@ -0,0 +1,20 @@
+/*
+ * sem_os_APP.h
+ *
+ *  Created on: 2018Äê6ÔÂ13ÈÕ
+ *      Author: Administrator
+ */
+//#pragma once
+#include "common.h"
+
+
+
+#ifndef _SEM_OS_APP_H_
+#define _SEM_OS_APP_H_
+
+static mico_semaphore_t os_sem = NULL;
+//int semphr_fd = -1;
+
+
+
+#endif /* DEMOS_HELLOWORLD_SEM_OS_APP_H_ */
